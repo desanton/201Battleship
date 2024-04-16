82cfb1d43ab12b1aee5495253d143d21b476f6ea
erika lee <erikal@usc.edu>
2024-04-16 12:51:45 -0700

    update coordinates class
    adding status

--- src/backend/main/java/com/Battleship/Coordinates.java
+++ src/backend/main/java/com/Battleship/Coordinates.java
@@ -6,13 +6,24 @@ public class Coordinates {
     // y coordinate
     int y;
 
+    // status of coordinate
+    /*
+     * 0 = empty
+     * 1 = occupied
+     * 2 = sunken ship
+     * 3 = attacked empty
+     * 4 = attacked occupied
+     */
+    int s;
+
     /*
      * CONSTRUCTOR
      * param: 2 integers -> x and y value
      * set x & y values
      */
-    public Coordinate(int x, int y){
+    public Coordinates(int x, int y){
         this.x = x;
         this.y = y;
+        s = 0;
     }
 }
