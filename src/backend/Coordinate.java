6607f481ba1aa67a8b3dc08099c75d3e94e3debf
erika lee <erikal@usc.edu>
2024-04-16 11:38:29 -0700

    updating Coordinate class

--- src/backend/Coordinate.java
+++ src/backend/Coordinate.java
@@ -1,5 +1,15 @@
 package backend;
 
 public class Coordinate {
+    // x coordinate
+    int x;
     
+    // y coordinate
+    int y;
+
+    // sets coordinates given an x and y value
+    public void setCoordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
