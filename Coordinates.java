e80b6f80743451d90b735ad77f8f6ebf3def528c
erika lee <erikal@usc.edu>
2024-04-16 00:23:37 -0700

    Ship & Coordinate Classes [1st Draft]
    probably will need to update them as we code the other parts of the game

---
+++ Coordinates.java
@@ -0,0 +1,13 @@
+public class Coordinates {
+    // x coordinate
+    int x;
+    
+    // y coordinate
+    int y;
+
+    // sets coordinates given an x and y value
+    public void setCoordinates(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
