abdd423c2d879719c5cdfebc6475cb83912fa3f0
Michael Wang <michaelwang4161@gmail.com>
2024-04-16 12:50:22 -0700

    Implemented login backend

--- backend/src/main/java/com/battleship201/dao/UserDAO.java
+++ backend/src/main/java/com/battleship201/dao/UserDAO.java
@@ -19,4 +19,17 @@ public class UserDAO {
             return false;
         }
     }
+
+    public boolean checkUser(User user) {
+        String sql = "SELECT * FROM Users WHERE Username = ? AND Password = ?";
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            return statement.executeQuery().next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
