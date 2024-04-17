cdf86244cba2394536cdbe1ce04a34877aaed88f
Hannah Nguyen <nnguyen3@usc.edu>
2024-04-16 21:09:45 -0700

    Update User.java

--- src/backend/main/java/com/Battleship/User.java
+++ src/backend/main/java/com/Battleship/User.java
@@ -6,13 +6,22 @@ public class User extends Player {
     }
     
     // used for the opponent/computer to attack user board
-    public boolean getAttack(Coordinate C) {
+    public boolean setUserAttack(Coordinate C) {
         
     }
 
     // used to get coordinate from user and attack opponent board
-    // have to get coordinate from front end
-    public boolean setAttack() {
+    public boolean getUserAttack() {
+        Coordinate c;
+        // get user attack
+            // TODO: add logic to get coordinate from front end user input
+            
+            // TODO: check if coordinates are within bounds
+
+        
+
+        // attack opponent board
+        return setOpponentAttack(c);
 
     }
 }
