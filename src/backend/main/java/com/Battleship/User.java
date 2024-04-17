8a24519e804ed332987cbcbc73d73b698bc04ba0
Hannah Nguyen <nnguyen3@usc.edu>
2024-04-16 20:58:26 -0700

    updated attack logic

--- src/backend/main/java/com/Battleship/User.java
+++ src/backend/main/java/com/Battleship/User.java
@@ -5,7 +5,14 @@ public class User extends Player {
         super();
     }
     
-    public boolean attack(Coordinate C) {
+    // used for the opponent/computer to attack user board
+    public boolean getAttack(Coordinate C) {
         
+    }
+
+    // used to get coordinate from user and attack opponent board
+    // have to get coordinate from front end
+    public boolean setAttack() {
+
     }
 }
