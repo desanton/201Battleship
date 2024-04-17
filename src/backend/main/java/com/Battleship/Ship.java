008d690d2851fe965e84bf0c6a8112d597dfb9c8
Hannah Nguyen <nnguyen3@usc.edu>
2024-04-16 21:20:09 -0700

    Update Ship.java

--- src/backend/main/java/com/Battleship/Ship.java
+++ src/backend/main/java/com/Battleship/Ship.java
@@ -7,11 +7,25 @@ public class Ship {
 
     // in units of game board (i.e. destroyer occupies 2 spots on game board, so size = 2)
     int size;
+
+    // number of hits on the ship
+    int hitCount;
     
     // set of all the coordinates that the boat takes up
     Set<Coordinates> coordinates;
 
+    // constructor
+    public Ship() {
+        hitCount = 0;
+    }
+
     public Set<Coordinates> getShipCoordinates(String type){
         return coordinates;
+    }
+
+    // returns true if ship is sunk 
+    public boolean updateHitCount() {
+        hitCount++;
+        return (hitCount == size);
     }
 }
