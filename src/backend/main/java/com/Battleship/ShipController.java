d40c6573b1432ba80e7ed19157c79de6083b9c0e
Hannah Nguyen <nnguyen3@usc.edu>
2024-04-24 10:51:26 -0700

    Update ShipController.java

--- src/backend/main/java/com/Battleship/ShipController.java
+++ src/backend/main/java/com/Battleship/ShipController.java
@@ -4,6 +4,7 @@ public class ShipController {
     // create user & opponent objects
     private User user = new User();
     private Opponent opponent = new Opponent();
+    public static boolean endGame;
 
     // total number of ships for 1 player
     private final int totalNumShips = 5;
@@ -86,6 +87,56 @@ public class ShipController {
 
     public void endGame() {
 
+    }
+
+    public static void main(String[] args) {
+		
+		endGame = false; // game just started
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Initializing user");
+        User user = new User();
+        
+        System.out.println("Initializing opponent");
+        Opponent opp = new Opponent(user);
+
+        while (!endGame) {
+        	
+            System.out.println("Enter coordinates to attack (x y)");
+            String input = scan.nextLine();
+            
+            
+            // split the input into x and y coordinates
+            String[] coordinates = input.split(" ");
+            
+            // invalid input
+            if (coordinates.length != 2) {
+                System.out.println("Invalid input. Please enter two coordinates separated by space.");
+                continue; 
+            }
+            
+            try {
+            	
+                int x = Integer.parseInt(coordinates[0]);
+                int y = Integer.parseInt(coordinates[1]);
+                
+                // check if input is in bounds
+                if (x < 0 || x > 9 || y < 0 || y > 9) {
+	            	System.out.println("Invalid input. Please enter coordinates within bounds.");
+	                continue; 
+                } else {
+					opp.setOpponentAttack(new Coordinates(x, y));
+					opp.board.displayBoard();
+                }
+            
+            } catch (NumberFormatException e) {
+            	// not a valid number
+                System.out.println("Invalid input. Please enter valid integer coordinates.");
+                continue; 
+            }
+        }
+        
+        System.out.println("Exiting game. Goodbye!");
+        scan.close();
     }
 }
 
