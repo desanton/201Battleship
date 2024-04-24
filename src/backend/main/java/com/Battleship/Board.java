db0919f3684963523ab1119fce7f958ed919a51e
Hannah Nguyen <nnguyen3@usc.edu>
2024-04-24 10:45:49 -0700

    Update Board.java

--- src/backend/main/java/com/Battleship/Board.java
+++ src/backend/main/java/com/Battleship/Board.java
@@ -14,18 +14,18 @@ public class Board {
         board = new Coordinates[10][10]; 
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
-                board[x][y] = Coordinates(x, y);
+                board[x][y] = new Coordinates(x, y);
             }
         }
     }
 
-    public placeShipOnBoard(Coordinates C1, Coordinates C2) {
+    public void placeShipOnBoard(Coordinates C1, Coordinates C2) {
         // assuming the coordinates are within bounds and conflict with other pre-existing ships
-        int x1 = C1.x;
+    	int x1 = C1.x;
     	int x2 = C2.x;
     	int y1 = C1.y;
     	int y2 = C2.y;
-        
+    	
         if (x1 == x2) {
             for (int y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
                 board[x1][y].s = 1;
@@ -40,15 +40,16 @@ public class Board {
         }
     }
 
-    public updateCoordStatus(Coordinates c, int s){ 
+    public void updateCoordStatus(Coordinates c, int s){ 
         board[c.x][c.y].setStatus(s);
+
     }
 
     // for backend testing
     public void displayBoard() {
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
-                System.out.print(board[x][y] + " ");
+                System.out.print(board[x][y].s + " ");
             }
             System.out.println();
         }
