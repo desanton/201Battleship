9be802f7a5796fa0af18d64dbc9c3be0b98450b4
Michael Wang <michaelwang4161@gmail.com>
2024-04-26 21:27:24 -0700

    Removing old project code

--- backend/src/main/java/com/battleship201/service/PasswordHashing.java
+++
@@ -1,9 +0,0 @@
-package com.battleship201.service;
-
-import org.mindrot.jbcrypt.BCrypt;
-
-public class PasswordHashing {
-    public static String hashPassword(String plainTextPassword){
-        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
-    }
-}
--- src/backend/main/java/com/Battleship/BattleshipRepository.java
+++
@@ -1,6 +0,0 @@
-package backend.main.java.com.Battleship;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BattleshipRepository extends JpaRepository<Game, String> {
-}
